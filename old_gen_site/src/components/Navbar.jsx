ea404052398a4c64642f0e7d0bbdbc01a7315b0a
ousemaakacha <akachaousema@gmail.com>
2026-01-14 11:08:24 +0100

    feat: added logo to navbar

--- old_gen_site/src/components/Navbar.jsx
+++ old_gen_site/src/components/Navbar.jsx
@@ -4,7 +4,18 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="container navbar-container">
-                <Link className="navbar-brand" to="/">Metal Games Solid Shop</Link>
+                <div className="d-flex align-items-center gap-2">
+                <Link className="navbar-brand " to="/">METAL GAMES SOLID SHOP</Link>
+                    <img
+                        src="/logo.png"
+                        alt="Logo"
+                        width="60"
+                        height="60"
+                         className="d-inline-block align-text-top"
+                         
+                    />
+
+                </div>
                 <Link className="cart-icon" to="/cart">
                     <i className="bi bi-cart3"></i>
                 </Link>
