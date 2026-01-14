c4d47c06c5db2d099421a769f381ef611bc6de45
ousemaakacha <akachaousema@gmail.com>
2026-01-14 11:22:07 +0100

    fix: moved logo position i navbar

--- old_gen_site/src/components/Navbar.jsx
+++ old_gen_site/src/components/Navbar.jsx
@@ -5,15 +5,17 @@ export default function Navbar() {
         <nav className="navbar">
             <div className="container navbar-container">
                 <div className="d-flex align-items-center gap-2">
-                <Link className="navbar-brand " to="/">METAL GAMES SOLID SHOP</Link>
+                    <a href="/">
                     <img
                         src="/logo.png"
                         alt="Logo"
                         width="60"
                         height="60"
-                         className="d-inline-block align-text-top"
+                        className="d-inline-block align-text-top"
                          
                     />
+                    </a>
+                <Link className="navbar-brand" to="/">METAL GAMES SOLID SHOP</Link>
 
                 </div>
                 <Link className="cart-icon" to="/cart">
