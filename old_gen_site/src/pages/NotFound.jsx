4b7f6fc53f63567ccf334e5609fac196a70a2f62
stefanobellomo <stefanobellomo1998@gmail.com>
2026-01-19 11:49:52 +0100

    fix NotFound

--- old_gen_site/src/pages/NotFound.jsx
+++ old_gen_site/src/pages/NotFound.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
     return (
-        <main>
-            <div className="container">
-                <h1>404</h1>
+        <main className="not-found-page">
+            <div className="container not-found-content">
+                <h1>404 error found, dai torniamo a Pianilungone a fare scorta</h1>
+
+                <img className="not-found-img" src="/404.jpg" alt="error_found" />
+
+                <Link className="back" to="/">
+                    <h3>cliccami e torniamo alla Contea</h3>
+                </Link>
             </div>
         </main>
     );
